4e6f6670bf52850f30035d8dee870cf16bdbd071
katri96 <katru3@hotmail.com>
2017-09-15 23:54:06 +0300

    testi attribuutteja

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -6,4 +6,8 @@ INSERT INTO Aine (name) VALUES ('Banaani');
 INSERT INTO Aine (name) VALUES ('Suklaa');
 INSERT INTO Resepti (name, type, price, ohje) VALUES ('Suklaabanaani', 'Jälkiruoka', 4, 'Avaa banaani ja insert suklaa. Grillaa 10 min');
 
-INSERT INTO Kauttaja (name, password) VALUES ('Leipuri12', 'lolwut');
+INSERT INTO Kayttaja (name, password) VALUES ('Leipuri12', 'lolwut');
+
+INSERT INTO Arviointi (INTEGER) VALUES (2);
+
+INSERT INTO Resepti (name) VALUES ('Porkkanakakku');
